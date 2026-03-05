a504acbd2d48e695204f0c2430367bb340f8d7b0
Andrei Dietrich <169064248+Gesshokuuyu@users.noreply.github.com>
2026-03-05 19:40:51 -0300

    Volume Calc

---
+++ volume.py
@@ -0,0 +1,8 @@
+print('---------------------Calculo de Volume Retangular---------------------')
+ 
+comp = float(input('Insira o comprimento da caixa'))
+larg = float(input('Insira a largura da caixa'))
+altu = float(input('Insira a altura da caixa'))
+
+volume = comp * larg * altu
+print('O volume é', volume)
