8248187902c02e0eea01e49eee8414e5dc818ecd
Andrei Dietrich <169064248+Gesshokuuyu@users.noreply.github.com>
2026-03-05 19:45:02 -0300

    Trocando valores com ','

---
+++ InversaoValores.py
@@ -0,0 +1,10 @@
+print('--------------------Inversao de Valores--------------------')
+ 
+A = int(input('Insira o valor de A'))
+B = int(input('Insira o valor da B'))
+
+A,B = B,A
+
+print(f'Os valores invertidos são A:{A} e B:{B}')
+
+
