61975df02794156a87b484c532662abb18c3dfc9
ANDREI LUCIANO DIETRICH <andrei.dietrich@univille.br>
2026-03-19 19:42:24 -0300

    exercicio impar par

---
+++ exercicios-condicoes-4.py
@@ -0,0 +1,8 @@
+# Faça um algoritmo que leia um no inteiro e mostre uma mensagem indicando se este
+# número é par ou ímpar.
+number = int(input('Informe um número: '))
+
+if number % 2 == 0:
+    print('O numero é par')
+else:
+    print('O numero é impar')
