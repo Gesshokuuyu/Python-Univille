7a206762bb7439bc561474fd5a12aa9989efde3e
ANDREI LUCIANO DIETRICH <andrei.dietrich@univille.br>
2026-03-19 19:24:53 -0300

    second part of conditions

---
+++ exercicios-condicoes-2.py
@@ -0,0 +1,10 @@
+# Faça um algoritmo que leia um no inteiro e mostre uma mensagem indicando se este
+# número é positivo ou negativo.
+
+number = int(input('Informe um numero: '))
+
+if number > 0:
+    print(f'O número {number} é positivo')
+else:
+    print(f'O número {number} é negativo')
+    
